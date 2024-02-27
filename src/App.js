b671c3d0113b228b8f34cb6e244fe546be4f2eb6
AdityaRam1607 <102134127+AdityaRam1607@users.noreply.github.com>
2024-02-27 23:36:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './Netartcomponent.js'
+import NetartComponent from './Netartcomponent.js';
+function App() {
+  return (
+    <div className="App">
+     <NetartComponent/>
+    </div>
+  );
+}
+
+export default App;
